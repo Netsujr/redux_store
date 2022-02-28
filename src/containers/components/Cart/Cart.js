9aa17c3c64624721ae09e78a714dd30e614cb955
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-28 18:08:37 +0900

    useEffect Working?

--- src/containers/components/Cart/Cart.js
+++ src/containers/components/Cart/Cart.js
@@ -6,7 +6,18 @@ const Cart = ({ cart }) => {
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalItems, setTotalItems] = useState(0);
 
+  useEffect(() => {
+    let items = 0;
+    let price = 0;
 
+    cart.forEach((item) => {
+      items += item.qty;
+      price += item.qty * item.price;
+    });
+
+    setTotalItems(items);
+    setTotalPrice(price);
+  }, [cart, totalPrice, totalItems, setTotalPrice, setTotalItems]);
 
   return (
     <div >
