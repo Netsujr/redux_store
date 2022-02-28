a51638d5ffd404a7b1b331651c93c7ea5799c84e
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-28 18:08:24 +0900

    added CART

--- src/containers/components/Cart/Cart.js
+++ src/containers/components/Cart/Cart.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+import CartItem from "./CartItem/CartItem";
+const Cart = ({ cart }) => {
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
 
-const Cart = () => {
+
+
   return (
-    <div>
-      Cart
+    <div >
+      <div >
+        {cart.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
+      <div >
+        <h4 > >Cart Summary</h4>
+        <div >
+          <span>TOTAL: ({totalItems} items)</span>
+          <span>$ {totalPrice}</span>
+        </div>
+        <button >
+          Proceed To Checkout
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Cart;
+const mapStateToProps = (state) => {
+  return {
+    cart: state.shop.cart,
+  };
+};
+
+export default connect(mapStateToProps)(Cart);
