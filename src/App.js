0ccd58cb8cfd950a159b5df068d0dfe043cdb228
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-28 11:15:50 +0900

    added Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductList from './containers/ProductList';
+import Product from './containers/components/Product';
+import ProductDetails from './containers/components/Product';
 
 function App() {
   return (
     <div className="App">
-     Helo
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<ProductList/>} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
