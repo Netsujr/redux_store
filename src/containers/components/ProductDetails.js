b901cef24010b7626920809e0a903ad9d256abff
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-28 12:24:54 +0900

    added selected product reducer

--- src/containers/components/ProductDetails.js
+++ src/containers/components/ProductDetails.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { selectedProduct } from '../redux/actions/productActions';
+import { useDispatch, useSelector } from 'react-redux';
 
 const ProductDetails = () => {
+  const product = useSelector((state) => state.product);
+  const { productId } = useParams();
+  const dispatch = useDispatch();
+  const fetchProductDetail = async () => {
+    const response = await axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .catch(error => console.log(error));
+    dispatch(selectedProduct(response.data));
+  };
+
   return (
     <div>
       <h1>ProductDetails</h1>
