64d1acdb2f109661b4dfc60549f90d39e672c39e
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-28 18:15:48 +0900

    added style

--- src/containers/components/Cart/Cart.js
+++ src/containers/components/Cart/Cart.js
@@ -49,10 +49,68 @@ const mapStateToProps = (state) => {
 export default connect(mapStateToProps)(Cart);
 
 const CartContainer = styled.div`
-`;
+    margin: 2rem auto;
+    width: 1100px;
+    display: flex;
+    justify-content: space-between;
+    /* border: 1px solid red; */
+
+    button {
+      padding: 10px 17px;
+      border: 1px solid var(--secondary-color);
+      border-radius: 10px;
+      background: var(--primary-color);
+      color: var(--light-color);
+      font-size: 1.2rem;
+      font-weight: bold;
+      cursor: pointer;
+
+      &:focus {
+        outline: none;
+        border: 1px solid var(--secondary-color);
+      }
+
+      &:hover {
+        opacity: 0.75;
+      }
+    }
+    `;
 
 const CartItems = styled.div`
-`;
+    flex: 0.7;
+    margin-right: 1rem;
+    /* border: 1px solid pink; */
+    `;
 
 const CartSummary = styled.div`
-`;
+    height: 250px;
+    flex: 0.3;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 10px;
+
+    h4 {
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
+    `;
+
+const Price = styled.div`
+    display: flex;
+    align-items: center;
+    /* border: 1px solid blue; */
+
+    span:nth-child(1) {
+      font-size: 0.9rem;
+      flex: 0.6;
+    }
+
+    span:nth-child(2) {
+      flex: 0.4;
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
+    `
