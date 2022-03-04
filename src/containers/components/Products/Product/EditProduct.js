7180d67dc911dc025135d5429cc0bc3bb7ea59c4
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-04 10:18:46 +0900

    edit form looks good + works

--- src/containers/components/Products/Product/EditProduct.js
+++ src/containers/components/Products/Product/EditProduct.js
@@ -36,68 +36,123 @@ const EditProduct = () => {
 
   return (
     <>
-      <h1 style={{ marginLeft: '60px' }}>Edit Product</h1>
-      <ProductContainer>
-        <form onSubmit={handleSubmit} className="form">
-          <img src={selectedProduct.image} alt={selectedProduct.title} />
-          <ProductDetails>
-            <label htmlFor="title">Title</label>
-            <textarea
-              type="text"
-              wrap='soft'
-              value={selectedProduct?.title ? selectedProduct.title : ''}
-              onChange={(e) => handleChange('title', e.target.value)}
-              placeholder="Product Title"
-              title='title'
-            />
+    <ProductContainer>
+    <TitleImage>
+    <h1>Edit Product</h1>
+    <img src={selectedProduct.image} alt={selectedProduct.title} />
+    </TitleImage>
+    <ProductDetails>
+    <form className="form">
+    <label htmlFor="title">Title</label>
+    <textarea
+    type="text"
+    wrap='soft'
+    value={selectedProduct?.title ? selectedProduct.title : ''}
+    onChange={(e) => handleChange('title', e.target.value)}
+    placeholder="Product Title"
+    title='title'
+    />
 
-            <label htmlFor="description">Description</label>
-            <textarea
-              type="text"
-              value={selectedProduct?.description ? selectedProduct.description : ''}
-              onChange={(e) => handleChange('description', e.target.value)}
-              placeholder="Product Description"
-              name='description'
-            />
-
-            <input
-              type="number"
-              value={selectedProduct?.price ? selectedProduct.price : ''}
-              onChange={(e) => handleChange('price', e.target.value)}
-              placeholder="Price"
-              name='price'
-            />
-          </ProductDetails>
-          <ButtonsContainer>
-            <button>Submit</button>
-            <Link to='/'>Home</Link>
-          </ButtonsContainer>
-        </form>
-      </ProductContainer>
+    <label htmlFor="description">Description</label>
+    <textarea
+    type="text"
+    value={selectedProduct?.description ? selectedProduct.description : ''}
+    onChange={(e) => handleChange('description', e.target.value)}
+    placeholder="Product Description"
+    name='description'
+    />
+    <label htmlFor="price">Price</label>
+    <input
+    type="number"
+    value={selectedProduct?.price ? selectedProduct.price : ''}
+    onChange={(e) => handleChange('price', e.target.value)}
+    placeholder="Price"
+    name='price'
+    />
+    </form>
+    <ButtonsContainer>
+    <button onClick={handleSubmit}>Submit</button>
+    <Link
+    style={{ color: 'green', fontWeight: 'bold', fontSize: '20px' }}
+    to='/' >Back</Link>
+    </ButtonsContainer>
+    </ProductDetails>
+    </ProductContainer>
     </>
-  );
-};
+    );
+  };
 
-export default EditProduct;
+  export default EditProduct;
 
-const ButtonsContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  padding: 10px;
-  `;
-
-const ProductContainer = styled.div`
+  const ProductContainer = styled.div`
   width: 1100px;
   margin: 2rem auto;
   display: flex;
+  /* flex-direction: column; */
 
   .form {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    flex-direction: column;
+    margin: 1rem auto;
+
+    textarea {
+      font-size: 1rem;
+      min-height: 100px;
+      min-width: 500px;
+      text-align: start;
+      text-wrap: normal;
+      border: 1px solid var(--secondary-color);
+      border-radius: 10px;
+      padding: 1rem;
+      margin-bottom: 1rem;
+    }
+
+    input {
+      font-size: 1.2rem;
+      font-weight: bold;
+      height: 60px;
+      width: 150px;
+      padding: 1rem;
+      border: 1px solid var(--secondary-color);
+      border-radius: 10px;
+      text-align: center;
+    }
   }
+  `;
+
+  const TitleImage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+
+  img {
+    height: 400px;
+    object-fit: contain;
+    margin-right: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 10px;
+    padding: 2rem;
+  }
+  `;
+
+  const ProductDetails = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: var(--secondary-color);
+  `;
+
+  const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 10px;
 
   button {
     width: 45%;
@@ -114,35 +169,5 @@ const ProductContainer = styled.div`
     &:hover {
       opacity: 0.75;
     }
-  }
-
-  img {
-    height: 400px;
-    object-fit: contain;
-    margin-right: 1rem;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    border-radius: 10px;
-    padding: 2rem;
-  }
-  `;
-
-const ProductDetails = styled.div`
-  padding: 1rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  color: var(--secondary-color);
-
-  textarea:nth-child(1), textarea:nth-child(2) {
-    font-size: 1.4rem;
-    height: 100px;
-    width: 500px;
-    text-align: start;
-    text-wrap: normal;
-  }
-
-  input {
-    font-size: 1.2rem;
-    font-weight: bold;
   }
   `;
