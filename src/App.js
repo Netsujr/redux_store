aa11def0f0e3c513b0610267d9d9864d1188fed5
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-04 10:27:16 +0900

    path created

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 import Header from "./containers/Header";
 import Products from "./containers/components/Products/ProductList";
 import Cart from "./containers/components/Cart/Cart";
-// import AddProduct from "./containers/components/Products/Product/AddProduct";
+import AddProduct from "./containers/components/Products/Product/AddProduct";
 import EditProduct from "./containers/components/Products/Product/EditProduct";
 import ThankYou from "./containers/components/ThankYou";
 import SingleProduct from "./containers/components/SingleProduct/SingleProduct";
@@ -19,7 +19,7 @@ function App({ current }) {
         <Routes>
           <Route exact path="/" element={<Products/>} />
           <Route exact path="/cart" element={<Cart/>} />
-          {/* <Route exact path="/addProduct" element={<AddProduct/>} /> */}
+          <Route exact path="/addProduct" element={<AddProduct/>} />
           <Route exact path="/product/edit/:id" element={<EditProduct/>} />
           <Route exact path="/product/:id" element={<SingleProduct/>} />
           <Route exact path="/thanks" element={<ThankYou/>} />
---
+++ src/containers/components/Products/Product/AddProduct.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import { createProduct } from "../../../../redux/Store/storeActions";
+import Image from 'react-random-image';
+
+
+const AddProduct = () => {
+  return (
+    <div>
+Hello
+    </div>
+  );
+};
+
+export default AddProduct;
